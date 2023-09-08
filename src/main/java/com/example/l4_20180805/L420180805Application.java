8c50e4b998b1e4b04c48fb447661818ba28e951d
Daniel <a20180805@pucp.edu.pe>
2023-09-07 20:41:23 -0500

    base

---
+++ src/main/java/com/example/l4_20180805/L420180805Application.java
@@ -0,0 +1,13 @@
+package com.example.l4_20180805;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L420180805Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L420180805Application.class, args);
+	}
+
+}
